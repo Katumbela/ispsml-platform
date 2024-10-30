eee598c3d97372ba47ca00452ffc677fc23ff011
Joao Afonso Katombela <ja3328173@gmail.com>
2024-10-30 18:12:49 +0100

    feat: src/components/input/style.ts. Criação de novo arquivo

---
+++ src/components/input/style.ts
@@ -0,0 +1,14 @@
+
+export const baseInputClasses = 'input-base border py-1 rounded px-3 focus-within:text-primary focus-within:bg-blue-100/50 transition-all';
+export const primaryInputClasses = 'input-primary border-blue-500';
+export const secondaryInputClasses = 'input-secondary border-gray-500';
+export const disabledInputClasses = 'input-disabled bg-gray-200 cursor-not-allowed';
+
+export const defaultInputClasses = 'border-gray-300 border-2 focus-within:border-black outline-none  px-4 py-2 w-full';
+
+export const inputWrapperClasses = 'input-wrapper';
+export const inputLabelClasses = 'input-label';
+export const inputContainerClasses = 'input-container flex items-center';
+export const inputIconClasses = 'input-icon flex items-center';
+export const leftIconClasses = 'left-icon';
+export const rightIconClasses = 'right-icon';

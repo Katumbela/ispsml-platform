f5aeb165083f7f6f804f5ec3588ab408d239f630
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-07 19:08:13 +0100

    feat: src/components/Card.tsx. Criação de novo arquivo

---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+interface CardProps {
+  image: string;
+  title: string;
+  description: string;
+}
+
+const Card: React.FC<CardProps> = ({ image, title, description }) => {
+  return (
+    <div className="bg-white shadow-md rounded-lg overflow-hidden">
+      <Image src={image} width={100} height={100} alt={title} className="w-full h-48 object-cover" />
+      <div className="p-4">
+        <h3 className="text-xl font-bold mb-2">{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;

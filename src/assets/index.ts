314cefca4ac495b537adebc15d4fb6bb539bc12f
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-07 19:08:09 +0100

    feat: src/assets/index.ts. Criação de novo arquivo

---
+++ src/assets/index.ts
@@ -0,0 +1,35 @@
+
+// Logos
+import logo1 from './logos/logo.png';
+
+// Teachers
+import teacher1 from './teachers/teacher-1.jpeg';
+import teacher2 from './teachers/teacher-2.jpg';
+import teacher3 from './teachers/teacher-3.png';
+import teacher4 from './teachers/teacher-4.jpeg';
+
+// Backgrounds
+import bg1 from './bgs/school-bg-1.jpeg';
+
+
+export const logos = {
+  logo1,
+};
+
+export const teachers = {
+  teacher1,
+  teacher2,
+  teacher3,
+  teacher4,
+};
+
+export const backgrounds = {
+  bg1,
+};
+
+
+export const images = {
+  logos,
+  teachers,
+  backgrounds,
+};

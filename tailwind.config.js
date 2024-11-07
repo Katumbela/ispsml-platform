0ea59f24ec190477538dd9bbe27e210986d3ed13
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-07 19:48:14 +0100

    fix: tailwind.config.js. Modificação de código em tailwind.config.js; Criação de uma constante em tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,17 @@ const config = {
           dark: '#F57C00',
         },
       },
+      container: {
+        center: true,
+        padding: "15px",
+        screens: {
+          sm: "576px",
+          md: "768px",
+          lg: "992px",
+          xl: "1200px",
+          "2xl": "1400px",
+        },
+      },
     },
   },
   plugins: [],

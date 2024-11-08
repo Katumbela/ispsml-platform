ad603607fcae7cc454c6c7b458192b55f8948347
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-08 12:12:31 +0100

    fix: src/components/Counter.tsx. Modificação de código em src/components/Counter.tsx; Alteração de texto em src/components/Counter.tsx; Criação de uma constante em src/components/Counter.tsx; Criação de uma variável em src/components/Counter.tsx; Mudança de texto em um elemento HTML em src/components/Counter.tsx; Alteração de conteúdo em array ou objeto em src/components/Counter.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,6 +1,8 @@
 "use client"
-import { useState, useEffect } from 'react';
+
+import { useState, useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
+import CountUp from 'react-countup';
 
 interface CounterProps {
   end: number;
@@ -9,25 +11,53 @@ interface CounterProps {
 }
 
 const Counter: React.FC<CounterProps> = ({ end, duration, label }) => {
-  const [count, setCount] = useState(0);
+  
+  const [isInView, setIsInView] = useState(false);
+  const counterRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    let start = 0;
-    const increment = end / (duration / 1000);
-    const interval = setInterval(() => {
-      start += increment;
-      if (start >= end) {
-        clearInterval(interval);
-        start = end;
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsInView(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.1 }
+    );
+
+    if (counterRef.current) {
+      observer.observe(counterRef.current);
+    }
+
+    return () => {
+      if (counterRef.current) {
+        observer.unobserve(counterRef.current);
       }
-      setCount(Math.floor(start));
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [end, duration]);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isInView) {
+      let start = 0;
+      const increment = end / (duration / 1000);
+      const interval = setInterval(() => {
+        start += increment;
+        if (start >= end) {
+          clearInterval(interval);
+          start = end;
+        }
+        // setCount(Math.floor(start));
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [isInView, end, duration]);
 
   return (
-    <motion.div className="text-center" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
-      <h2 className="text-4xl font-bold">{count}</h2>
+    <motion.div ref={counterRef} className="text-center" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+      <h2 className="text-4xl font-bold">
+        <CountUp duration={4} className="counter" end={end} />
+      </h2>
       <p>{label}</p>
     </motion.div>
   );

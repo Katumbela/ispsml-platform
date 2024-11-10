496d2a51fe4e94525a0ebfe813bbf66287f93505
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-10 17:34:10 +0100

    fix: src/components/sections/admissions-section/FixedImage.tsx. Criação da função FixedImage em src/components/sections/admissions-section/FixedImage.tsx; Modificação de código em src/components/sections/admissions-section/FixedImage.tsx; Alteração em componente React em src/components/sections/admissions-section/FixedImage.tsx; Alteração de conteúdo em array ou objeto em src/components/sections/admissions-section/FixedImage.tsx

--- src/components/sections/admissions-section/FixedImage.tsx
+++ src/components/sections/admissions-section/FixedImage.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { images } from '@/assets';
+import cn from 'classnames';
 
 function FixedImage({ isFixed }: { isFixed: boolean }) {
   return (
     <motion.div
-      className={`flex-1 ${isFixed ? 'fixed' : 'sticky'} top-0 h-screen w-1/3`}
+      className={`col-span-6 grid items-center  ${isFixed ? 'fixed' : 'sticky'} top-0 h-screen `}
+      // className={`w-full grid items-center place-content-center ${isFixed ? 'fixed' : 'sticky'} top-0 h-screen w-1/3`}
       initial={{ opacity: 0 }}
       animate={{ opacity: isFixed ? 1 : 1 }}
       transition={{ duration: 0.5 }}
     >
-      <Image src={images.backgrounds.bg_vertical} alt="Admissões" className="w-full h-full" />
+      <Image src={images.backgrounds.bg_vertical} alt="Admissões" className="w-[50vh] h-[50vh]" />
     </motion.div>
   );
 }

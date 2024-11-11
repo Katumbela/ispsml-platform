d663b28652c72ba060f065d8266a99bb4768e91f
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-11 17:23:57 +0100

    feat: src/infra/routes.vars.ts. Criação de novo arquivo

---
+++ src/infra/routes.vars.ts
@@ -0,0 +1,13 @@
+const HOME_ROUTE = "/",
+  LOGIN_ROUTE = "/login",
+  REGISTER_ROUTE = "/register",
+  DASHBOARD_ROUTE = "/dashboard",
+  DET_ROUTER = "/ispml-det"
+
+  export const routes = {
+      HOME_ROUTE,
+      LOGIN_ROUTE,
+      REGISTER_ROUTE,
+      DASHBOARD_ROUTE,
+      DET_ROUTER
+  }

745aa11808894a69793af57b93c882fb75e59cdd
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-11 17:24:01 +0100

    fix: src/assets/index.ts. Modificação de código em src/assets/index.ts; Criação de uma constante em src/assets/index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -13,6 +13,8 @@ import bg1 from './bgs/school-bg-1.jpeg';
 import bg_vertical from './bgs/school-bg-3-vertical.jpeg';
 import bg_night_1 from './bgs/bg-night-1.jpeg';
 import bg_night_2 from './bgs/bg-night-2.webp';
+import bg_books_pen from './bgs/bg-book-pens.jpg';
+import bg_school_2 from './bgs/school-bg-2.jpg';
 
 
 // importando as imagens dos departamentos
@@ -20,6 +22,9 @@ import bg_night_2 from './bgs/bg-night-2.webp';
 import ciencias_sociais from './departs-bgs/ciencias-sociais.webp';
 import eng_tech from './departs-bgs/istockphoto-1154341677-612x612.jpg';
 import geoscience from './departs-bgs/geoscience.jpg';
+import studying_group from './departs-bgs/studying-group.avif';
+import studying_group_2 from './departs-bgs/studying-group.avif';
+import studying_group_random from './departs-bgs/istockphoto-1154341677-612x612.jpg';
 
 export const logos = {
   logo1,
@@ -36,13 +41,18 @@ export const backgrounds = {
   bg1,
   bg_night_1,
   bg_night_2,
-  bg_vertical,
+  bg_vertical,  
+  bg_books_pen,
+  bg_school_2,
 };
 
 export const departImages = {
   ciencias_sociais,
   eng_tech,
-  geoscience
+  geoscience,
+  studying_group,
+  studying_group_2,
+  studying_group_random,
 }
 
 export const images = {

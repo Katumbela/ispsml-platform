dbd8cf41deb94a45eb5d27350522f49d0cc6e001
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-11 18:40:07 +0100

    feat: src/app/ispml-det/components/eng-courses-section.tsx. Criação de novo arquivo

---
+++ src/app/ispml-det/components/eng-courses-section.tsx
@@ -0,0 +1,28 @@
+import { coursesData } from "@/infra/data/courses-data";
+import { routes } from "@/infra/routes.vars";
+import { FaAngleRight } from "react-icons/fa6";
+import { CardCourseComponent } from "./card-course-component";
+
+
+
+export function EngCoursesSection() {
+
+  return (
+    <div className="container courses-section">
+      <h1 className="text-3xl font-bold">
+        Cursos de Engenharia
+      </h1>
+      <br />
+      <br />
+
+      <div className="grid flex-wrap  grid-cols-12 gap-1.5">
+        {
+          coursesData.map((course, i) => (
+            <CardCourseComponent course={course} key={i} />
+          ))
+        }
+      </div>
+    </div>
+
+  )
+}

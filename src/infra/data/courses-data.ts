7adf6ee257dbf8903e6be9fdd69857985a22267c
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-11 18:40:11 +0100

    feat: src/infra/data/courses-data.ts. Criação de novo arquivo

---
+++ src/infra/data/courses-data.ts
@@ -0,0 +1,60 @@
+import type { ICourse } from "../interfaces/course.interface";
+
+export const coursesData: ICourse[] = [
+  {
+    course: "Engenharia Quimica",
+    duration: 5,
+    level: "graduation",
+    short_detail: "The best course ever",
+    slug: "chemical-engineering-graduation"
+  },
+  {
+    course: "Engenharia Eletrotécnica",
+    duration: 4,
+    level: "graduation",
+    short_detail: "The best course ever",
+    slug: "eletrotechnical-engineering-graduation"
+  },
+  {
+    course: "Engenharia Informática",
+    duration: 4,
+    level: "graduation",
+    short_detail: "The best course ever",
+    slug: "informatic-engineering-graduation"
+  },
+  {
+    course: " Ciência da Computação",
+    duration: 5,
+    level: "graduation",
+    short_detail: "The best course ever",
+    slug: "computer-science-graduation"
+  },
+  {
+    course: "Engenharia Quimica",
+    duration: 5,
+    level: "graduation",
+    short_detail: "The best course ever",
+    slug: "chemical-engineering-graduation"
+  },
+  {
+    course: "Engenharia Eletrotécnica",
+    duration: 4,
+    level: "graduation",
+    short_detail: "The best course ever",
+    slug: "eletrotechnical-engineering-graduation"
+  },
+  {
+    course: "Engenharia Informática",
+    duration: 4,
+    level: "graduation",
+    short_detail: "The best course ever",
+    slug: "informatic-engineering-graduation"
+  },
+  {
+    course: " Ciência da Computação",
+    duration: 5,
+    level: "graduation",
+    short_detail: "The best course ever",
+    slug: "computer-science-graduation"
+  }
+]

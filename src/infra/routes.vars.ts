c3f74ab411b38d214c83e67d2319c59a513ffe98
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-11 18:40:24 +0100

    fix: src/infra/routes.vars.ts. Modificação de código em src/infra/routes.vars.ts; Criação de uma constante em src/infra/routes.vars.ts

--- src/infra/routes.vars.ts
+++ src/infra/routes.vars.ts
@@ -2,12 +2,14 @@ const HOME_ROUTE = "/",
   LOGIN_ROUTE = "/login",
   REGISTER_ROUTE = "/register",
   DASHBOARD_ROUTE = "/dashboard",
+  VIEW_COURSE_ROUTE = "/course",
   DET_ROUTER = "/ispml-det"
 
-  export const routes = {
-      HOME_ROUTE,
-      LOGIN_ROUTE,
-      REGISTER_ROUTE,
-      DASHBOARD_ROUTE,
-      DET_ROUTER
-  }
+export const routes = {
+  HOME_ROUTE,
+  LOGIN_ROUTE,
+  VIEW_COURSE_ROUTE,
+  REGISTER_ROUTE,
+  DASHBOARD_ROUTE,
+  DET_ROUTER
+}

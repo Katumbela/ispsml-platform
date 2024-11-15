af08fdefe213cb80bad110fc4a25ba2d7edaebbb
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-15 19:27:52 +0100

    fix: src/infra/interfaces/course.interface.ts. Modificação de código em src/infra/interfaces/course.interface.ts

--- src/infra/interfaces/course.interface.ts
+++ src/infra/interfaces/course.interface.ts
@@ -1,34 +1,41 @@
 export interface ICourse {
-  id?: string
-  course: string
-  short_detail?: string
-  long_description?: string
-  duration?: number
-  level?: 'graduation' | 'master' | 'bacharel'
-  slug?: string
-  shift?: IShift
-  years: IYear[]
-  course_cover: string
+  id?: string;
+  course: string;
+  short_detail?: string;
+  long_description?: string;
+  duration?: number;
+  level?:
+    | "bacharel"
+    | "graduation"
+    | "professional"
+    | "master"
+    | "pos-graduation";
+  slug?: string;
+  shift?: IShift;
+  benefits?: string[];
+
+  years: IYear[];
+  course_cover: string;
   additional_courses?: ICourse[];
 }
 
 export interface IShift {
-  morning: boolean
-  afternoon: boolean
-  evening: boolean
+  morning: boolean;
+  afternoon: boolean;
+  evening: boolean;
 }
 
 export interface IYear {
-  year: number
-  semesters: ISemester[]
+  year: number;
+  semesters: ISemester[];
 }
 
 export interface ISemester {
-  semester: number
-  subjects: ISubject[]
+  semester: number;
+  subjects: ISubject[];
 }
 
 export interface ISubject {
-  name: string
-  workload: number // in hours
+  name: string;
+  workload: number; // in hours
 }

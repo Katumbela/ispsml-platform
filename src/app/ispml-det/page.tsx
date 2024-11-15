33cb220758079a5725d531c465f813421c851996
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-15 14:23:57 +0100

    fix: src/app/ispml-det/page.tsx. Modificação de código em src/app/ispml-det/page.tsx; Criação de uma constante em src/app/ispml-det/page.tsx

--- src/app/ispml-det/page.tsx
+++ src/app/ispml-det/page.tsx
@@ -10,7 +10,6 @@ const About = () => {
     <div>
       <Navbar />
       <HeroDet />
-
       <main className="container my-8">
         <p className="mt-4">
           O Departamento de Engenharia e Tecnologia da nossa instituição é dedicado a avançar as fronteiras do conhecimento e da inovação. Nossos programas são projetados para equipar os alunos com as habilidades e a expertise necessárias para se destacar em um cenário tecnológico em rápida evolução. Oferecemos uma variedade de cursos e oportunidades de pesquisa que cobrem várias áreas, como ciência da computação, engenharia elétrica, engenharia mecânica e mais. Nossos membros do corpo docente são especialistas renomados em suas áreas, comprometidos em fornecer educação de alta qualidade e promover um ambiente de aprendizado colaborativo. Junte-se a nós para fazer parte de uma comunidade que está moldando o futuro da engenharia e da tecnologia.

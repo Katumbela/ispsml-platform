b2c3a76f1e6773f61acc3031959a6a3666e9cfab
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-15 20:21:07 +0100

    fix: src/app/ispml-det/page.tsx. Modificação de código em src/app/ispml-det/page.tsx; Criação de uma constante em src/app/ispml-det/page.tsx; Alteração de conteúdo em array ou objeto em src/app/ispml-det/page.tsx

--- src/app/ispml-det/page.tsx
+++ src/app/ispml-det/page.tsx
@@ -1,15 +1,17 @@
 import Footer from '@/components/Footer';
-import Navbar from '../../components/Navbar';
-import HeroDet from './components/hero-det';
+import Navbar from '../../components/Navbar'; 
 import QuickLinks from '@/components/QuickLinks';
 import { PrepaSection } from './components/prepa-section';
 import { EngCoursesSection } from './components/eng-courses-section';
+import GlobalHero from '@/components/global-hero/global-hero';
+import { images } from '@/assets';
 
 const ISPMLDet = () => {
   return (
     <div>
-      <Navbar />
-      <HeroDet />
+      <Navbar /> 
+      <GlobalHero  className='h-[400px] 2xl:h-[50px]' bottomBG='dark' bgImage={images.backgrounds.bg_night_1.src} title="   Departamento de Engenharia de Tecnologias" />
+      
       <main className="container my-8">
         <p className="mt-4">
           O Departamento de Engenharia e Tecnologia da nossa instituição é dedicado a avançar as fronteiras do conhecimento e da inovação. Nossos programas são projetados para equipar os alunos com as habilidades e a expertise necessárias para se destacar em um cenário tecnológico em rápida evolução. Oferecemos uma variedade de cursos e oportunidades de pesquisa que cobrem várias áreas, como ciência da computação, engenharia elétrica, engenharia mecânica e mais. Nossos membros do corpo docente são especialistas renomados em suas áreas, comprometidos em fornecer educação de alta qualidade e promover um ambiente de aprendizado colaborativo. Junte-se a nós para fazer parte de uma comunidade que está moldando o futuro da engenharia e da tecnologia.

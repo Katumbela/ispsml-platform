4daacb7a39070055119b174bc44056b961ef220a
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-15 14:23:56 +0100

    fix: src/app/ispml-det/components/card-course-component.tsx. Criação da função CardCourseComponent em src/app/ispml-det/components/card-course-component.tsx; Modificação de código em src/app/ispml-det/components/card-course-component.tsx; Alteração de texto em src/app/ispml-det/components/card-course-component.tsx; Alteração em componente React em src/app/ispml-det/components/card-course-component.tsx; Mudança de texto em um elemento HTML em src/app/ispml-det/components/card-course-component.tsx; Alteração em funções de manipulação de eventos em src/app/ispml-det/components/card-course-component.tsx; Alteração de conteúdo em array ou objeto em src/app/ispml-det/components/card-course-component.tsx

--- src/app/ispml-det/components/card-course-component.tsx
+++ src/app/ispml-det/components/card-course-component.tsx
@@ -1,21 +1,20 @@
-
 "use client"
 import type { ICourse } from "@/infra/interfaces/course.interface";
 import { routes } from "@/infra/routes.vars";
 import { FaAngleRight } from "react-icons/fa6";
 interface CardCourseComponentProps {
-  course: ICourse
+  course: ICourse,
+  department: string
 }
 
-
-export function CardCourseComponent({ course }: CardCourseComponentProps) {
+export function CardCourseComponent({ course, department }: CardCourseComponentProps) {
   return (
-    <div onClick={() => window.location.href = routes.VIEW_COURSE_ROUTE + '/' + course.slug} className="grid col-span-2 card-view-course w-[15vw] h-[30vh] items-center text-white bg-primary hover:bg-primary-dark cursor-pointer transition-all place-content-center">
+    <div onClick={() => window.location.href = `${routes.VIEW_COURSE_ROUTE}/${department}/${course.slug}`} className="grid col-span-2 card-view-course w-[15vw] h-[30vh] items-center text-white bg-primary hover:bg-primary-dark cursor-pointer transition-all place-content-center">
       <div className="text-center whitespace-break-spaces">
         <h2 className="text-2xl mg-3">
           {course.course}
         </h2>
-        <a href={`${routes.VIEW_COURSE_ROUTE}/${course.slug}`} className="justify-center hidden gap-1 text-xs underline transition-all ">
+        <a href={`${routes.VIEW_COURSE_ROUTE}/${department}/${course.slug}`} className="justify-center hidden gap-1 text-xs underline transition-all ">
           <span className="my-auto">Ver programa</span>
           <FaAngleRight className="my-auto text-xs" />
         </a>

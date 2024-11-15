67cea342c3c92ebb935985df111623756b51f548
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-15 19:27:39 +0100

    fix: src/app/course/components/hero-course-details.tsx. Criação da função HeroCourseDetail em src/app/course/components/hero-course-details.tsx; Modificação de código em src/app/course/components/hero-course-details.tsx; Alteração de texto em src/app/course/components/hero-course-details.tsx; Alteração em componente React em src/app/course/components/hero-course-details.tsx; Mudança de texto em um elemento HTML em src/app/course/components/hero-course-details.tsx; Alteração de conteúdo em array ou objeto em src/app/course/components/hero-course-details.tsx

--- src/app/course/components/hero-course-details.tsx
+++ src/app/course/components/hero-course-details.tsx
@@ -8,16 +8,21 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { useEffect, useState } from 'react';
+import { FaLocationCrosshairs, FaRegClock } from 'react-icons/fa6';
+import { ICourse } from '@/infra/interfaces/course.interface';
+import { FaPencilRuler } from 'react-icons/fa';
+import { GetLevelDescription } from '@/utils/return-level-name';
  
 
 interface HeroCourseDetailProps {
   title?: string
   bg_image?: string | StaticImageData
   height?: string
+  course?: ICourse
 }
 
 
-export function HeroCourseDetail({ bg_image, title }: HeroCourseDetailProps) {
+export function HeroCourseDetail({ bg_image, title, course }: HeroCourseDetailProps) {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [animationDirection, setAnimationDirection] = useState('top');
 
@@ -49,7 +54,7 @@ export function HeroCourseDetail({ bg_image, title }: HeroCourseDetailProps) {
         <Slider {...settings}>
 
           <motion.div
-            className={`relative 2xl:h-[450px] h-[350px]  items-start grid place-content-end overflow-hidden`}
+            className={`relative 2xl:h-[500px] h-[450px]  items-start grid place-content-end overflow-hidden`}
             initial={{ scale: 0.9 }}
             animate={{ scale: 1 }}
             transition={{ duration: 0.5 }}
@@ -65,8 +70,10 @@ export function HeroCourseDetail({ bg_image, title }: HeroCourseDetailProps) {
             <div className="relative z-10 my-auto text-start">
               <div className="py-8 bg-gradient-to-t from-black ">
                 <div className="container">
+ 
+ <span>{GetLevelDescription(course?.level)}</span>
                   <motion.h1
-                    className="text-2xl font-bold md:text-4xl"
+                    className="text-2xl font-bold mb-[10vh] md:text-4xl"
                     initial={{ opacity: 0, y: getInitialY() }}
                     animate={{ opacity: 1, y: 0, scale: [1, 1.2, 1] }}
                     transition={{ duration: 0.5, delay: 0.5, type: 'spring', stiffness: 100 }}
@@ -74,6 +81,47 @@ export function HeroCourseDetail({ bg_image, title }: HeroCourseDetailProps) {
                   >
                     {title}
                   </motion.h1>
+                  <br />
+                  <br />
+
+                  {/* div onde tem os icones da div */}
+                  <div className="grid w-auto text-center grid-cols-3 p-2 max-w-[60vw] mx-auto place-content-center bg-primary">
+                    <div className='py-3'>
+                    <div className="flex justify-center gap-5 text-xl justify">
+                      
+                          <FaLocationCrosshairs className='my-auto text-3xl' />
+                         
+                        <div className='flex flex-col text-start'>
+                          <p>Ano Letivo</p>
+                          <span className="text-sm">2024 - 2025</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='py-3 border-x-2'>
+                      <div className="flex justify-center gap-5 text-xl justify">
+                       
+                          <FaRegClock className='my-auto text-3xl' />
+                     
+                        <div className='flex flex-col text-start'>
+                          <p>Duração</p>
+                          <span className="text-sm">{course?.duration} anos</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                    <div className="flex justify-center gap-5 py-3 text-xl justify">
+                        
+                          <FaPencilRuler className='my-auto text-3xl' />
+                         
+                  
+                        <div className='flex flex-col text-start'>
+                          <p>Modalidade</p>
+                          <span className="text-sm">{course?.duration} anos</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <br /> 
                 </div>
 
               </div>

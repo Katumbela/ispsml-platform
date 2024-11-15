eae8dd9d7000b7cfcd0b02ce961c1fc94464a73f
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-15 19:27:34 +0100

    fix: src/app/contact/page.tsx. Modificação de código em src/app/contact/page.tsx; Alteração de texto em src/app/contact/page.tsx; Criação de uma constante em src/app/contact/page.tsx; Mudança de texto em um elemento HTML em src/app/contact/page.tsx; Alteração de conteúdo em array ou objeto em src/app/contact/page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,25 @@
+"use client"
+
+import GlobalHero from '@/components/global-hero/global-hero';
 import Navbar from '../../components/Navbar';
+import { images } from '@/assets';
+import ContactComponent from './components/contact-component';
+import Footer from '@/components/Footer';
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
-      <main className="p-8">
-        <h1 className="text-2xl font-bold">Contato</h1>
-        <p className="mt-4">Informações de contato...</p>
+      <GlobalHero title='Como podemos ajudar ?' subtitle='Contante o ISPML' className='h-[500px] 2xl:h-[650px]' bottomBG='dark' bgImage={images.backgrounds.bg_support.src}/>
+      <main className="container">
+      
+      <ContactComponent />
+
+<br />
+<br />
+      
       </main>
+<Footer />
     </div>
   );
 };

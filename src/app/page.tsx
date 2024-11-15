19e36d12b249f30e2c486d000339cc974e11b255
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-15 20:21:08 +0100

    fix: src/app/page.tsx. Criação da função Home em src/app/page.tsx; Modificação de código em src/app/page.tsx; Alteração de texto em src/app/page.tsx; Alteração em componente React em src/app/page.tsx; Mudança de texto em um elemento HTML em src/app/page.tsx; Alteração de conteúdo em array ou objeto em src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 "use client"
 
-import Navbar from '../components/Navbar';
-import Card from '@/components/Card';
-import Counter from '@/components/Counter';
-import Hero from '@/components/Hero';
-import { teachers } from '@/assets';
+import Navbar from '../components/Navbar'; 
+import Hero from '@/components/Hero'; 
 import QuickLinks from '@/components/QuickLinks';
 import Footer from '@/components/Footer';
 import { StudyOffer } from '@/components/sections/study-offer/study-offer';
 import { AdmissionsSection } from '@/components/sections/admissions-section/admissions-section';
+import HoverCard from '@/components/hover-card/hover-card';
+import { images } from '@/assets';
 
 export default function Home() {
   return (
@@ -19,43 +18,39 @@ export default function Home() {
         <StudyOffer />
         <br />
         <AdmissionsSection />
-        <section className="py-20 bg-gray-100">
-          <div className="container mx-auto text-center">
-            <h2 className="mb-8 text-3xl font-bold">Escolas em números</h2>
-            <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
-              <Counter end={5000} duration={2} label="Estudantes" />
-              <Counter end={200} duration={2} label="Professores" />
-              <Counter end={50} duration={2} label="Cursos" />
-              <Counter end={10} duration={2} label="Faculdades" />
+        <section className="py-20 ">
+         <div className="flex gap-2">
+         <HoverCard
+            bgColor="bg-primary"
+            hoverBgColor="bg-primary"
+            title="Profesores Investigadores"
+            className='h-[400px] 2xl:h-[600px]'
+            bgImage={images.backgrounds.bg_investigator_1.src}
+          >
+            <div className="container px-6 text-lg text-white">
+              LA CIENCIA CON SOLUCIONES ACCIONABLES
+              <p className="text-sm">
+                Conoce el nuevo enfoque de investigación en el Tecnológico de Monterrey. Descubre los tres institutos y los diferentes Core Labs que cambiarán la forma de realizar investigación en México.
+              </p>
+              <br />
+              <a href="#" className="font-bold underline">Saber mais</a>
             </div>
-          </div>
-        </section>
-        <section className="py-20">
-          <div className="container mx-auto text-center">
-            <h2 className="mb-8 text-3xl font-bold">Eventos</h2>
-            <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
-              <Card image={teachers.teacher1.src} title="Evento 1" description="Descrição do evento 1." />
-              <Card image={teachers.teacher4.src} title="Evento 2" description="Descrição do evento 2." />
-              <Card image={teachers.teacher2.src} title="Evento 3" description="Descrição do evento 3." />
+          </HoverCard>
+         <HoverCard
+            bgImage={images.backgrounds.bg_ciencia_estudo_2.src}
+            className='h-[400px] 2xl:h-[600px]'
+            hoverBgColor="bg-red-500"
+            title="Laboratórios de ciencias e Medicina"
+          >
+            <div className="text-lg text-white">
+              LA CIENCIA CON SOLUCIONES ACCIONABLES
+              <p className="text-sm">
+                Conoce el nuevo enfoque de investigación en el Tecnológico de Monterrey. Descubre los tres institutos y los diferentes Core Labs que cambiarán la forma de realizar investigación en México.
+              </p>
             </div>
-          </div>
-        </section>
-        <section className="py-20 bg-gray-100">
-          <div className="container mx-auto text-center">
-            <h2 className="mb-8 text-3xl font-bold">Testemunhos</h2>
-            <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
-              <Card image={teachers.teacher3.src} title="Estudante 1" description="Testemunho do estudante 1." />
-              <Card image={teachers.teacher1.src} title="Estudante 2" description="Testemunho do estudante 2." />
-              <Card image={teachers.teacher4.src} title="Estudante 3" description="Testemunho do estudante 3." />
-            </div>
-          </div>
-        </section>
-        <section className="py-20">
-          <div className="container mx-auto text-center">
-            <h2 className="mb-8 text-3xl font-bold">Admissões e Oferta Educativa</h2>
-            <p className="mb-8 text-xl">Informações sobre admissões e a oferta educativa da Universidade Angolana.</p>
-            <a href="/admissions" className="px-4 py-2 text-white rounded bg-secondary">Saiba Mais</a>
-          </div>
+          </HoverCard>
+         </div>
+
         </section>
         <QuickLinks />
         <Footer />

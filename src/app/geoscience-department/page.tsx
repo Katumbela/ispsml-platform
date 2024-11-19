4131dcfdc6156a6cb778373100ec36e45937a52e
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-19 10:19:17 +0100

    fix: src/app/geoscience-department/page.tsx. Modificação de código em src/app/geoscience-department/page.tsx; Alteração de texto em src/app/geoscience-department/page.tsx; Criação de uma constante em src/app/geoscience-department/page.tsx; Mudança de texto em um elemento HTML em src/app/geoscience-department/page.tsx; Alteração de conteúdo em array ou objeto em src/app/geoscience-department/page.tsx

--- src/app/geoscience-department/page.tsx
+++ src/app/geoscience-department/page.tsx
@@ -29,12 +29,12 @@ const GeoScienceDepartment = () => {
       <Navbar /> 
       <GlobalHero className='h-[400px] 2xl:h-[50px]' bottomBG='primary' bgImage={images.departImages.geoscience.src} title="Departamento de GeoCiências" />
       
-      <main className="container my-8">
+      <main className="containers my-8">
         <p className="mt-4">
           O Departamento de GeoCiências da nossa instituição é dedicado a promover o conhecimento e a pesquisa nas áreas de ciências da terra. Nossos programas são projetados para equipar os alunos com as habilidades e a expertise necessárias para se destacar em diversas áreas, como geologia, geofísica, geografia, e mais. Oferecemos uma variedade de cursos e oportunidades de pesquisa que cobrem várias disciplinas. Nossos membros do corpo docente são especialistas renomados em suas áreas, comprometidos em fornecer educação de alta qualidade e promover um ambiente de aprendizado colaborativo. Junte-se a nós para fazer parte de uma comunidade que está moldando o futuro das ciências da terra.
         </p>
       </main>
-      <div className="container">
+      <div className="containers">
         <h2 className='mb-6 text-3xl font-bold'>Cursos</h2>
         <div className="grid gap-1.5 grid-cols-12">
           {coursesData.geoscience.courses.map((course, i) => (

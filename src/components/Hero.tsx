5dc2fc859994bda1849efeb183711fd1123e11a1
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-19 10:19:24 +0100

    fix: src/components/Hero.tsx. Modificação de código em src/components/Hero.tsx; Alteração de texto em src/components/Hero.tsx; Criação de uma constante em src/components/Hero.tsx; Mudança de texto em um elemento HTML em src/components/Hero.tsx; Alteração de conteúdo em array ou objeto em src/components/Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-"use client"
+'use client';
 
 import { motion } from 'framer-motion';
 import Image from 'next/image';
@@ -11,94 +11,95 @@ import { useEffect, useState } from 'react';
 import { carouselItems } from '@/infra/data/navbar-data';
 import { FaAngleRight } from 'react-icons/fa';
 
-
-
 const Hero = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [animationDirection, setAnimationDirection] = useState('top');
+	const [ currentSlide, setCurrentSlide ] = useState(0);
+	const [ animationDirection, setAnimationDirection ] = useState('top');
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    fade: true,
-    cssEase: 'linear',
-    beforeChange: (oldIndex: any, newIndex: any) => {
-      setCurrentSlide(newIndex);
-      setAnimationDirection(Math.random() > 0.5 ? 'top' : 'bottom');
-    },
-  };
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		fade: true,
+		cssEase: 'linear',
+		beforeChange: (oldIndex: any, newIndex: any) => {
+			setCurrentSlide(newIndex);
+			setAnimationDirection(Math.random() > 0.5 ? 'top' : 'bottom');
+		}
+	};
 
-  useEffect(() => {
-    // Trigger re-render to reset animations
-  }, [currentSlide]);
+	useEffect(
+		() => {
+			// Trigger re-render to reset animations
+		},
+		[ currentSlide ]
+	);
 
-  const getInitialY = () => (animationDirection === 'top' ? -50 : 50);
+	const getInitialY = () => (animationDirection === 'top' ? -50 : 50);
 
-  return (
-    <section className="text-white ">
-      <div className="pt-0 mx-auto text-center">
-        <Slider {...settings}>
-          {carouselItems.map((item, index) => (
-            <motion.div
-              key={index}
-              className="relative 2xl:h-[700px] h-[500px]  items-start grid place-content-end pb-10 overflow-hidden"
-              initial={{ scale: 0.9 }}
-              animate={{ scale: 1 }}
-              transition={{ duration: 0.5 }}
-            >
-              <Image
-                src={item.background.src}
-                alt={item.title}
-                layout="fill"
-                objectFit="cover"
-                className="absolute inset-0 z-[-1]"
-              />
-              <div className="absolute inset-0 bg-black opacity-50 z-[-1]" />
-              <div className="relative z-10 my-auto text-start">
-                <div className="container">
-                  <motion.h1
-                    className="mb-4 text-3xl font-bold md:text-5xl"
-                    initial={{ opacity: 0, y: getInitialY() }}
-                    animate={{ opacity: 1, y: 0, scale: [1, 1.2, 1] }}
-                    transition={{ duration: 0.5, delay: 0.5, type: 'spring', stiffness: 100 }}
-                    key={`title-${currentSlide}`}
-                  >
-                    {item.title}
-                  </motion.h1>
-                  <motion.p
-                    className="mb-4 text-lg md:text-xl"
-                    initial={{ opacity: 0, y: getInitialY() }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5, delay: 1 }}
-                    key={`description-${currentSlide}`}
-                  >
-                    {item.description}
-                  </motion.p>
-                  <motion.a
-                    href={item.link}
-                    className="flex gap-1 mb-4 font-bold tracking-wider text-md hover:underline"
-                    // className="px-6 py-3 text-lg font-bold transition-all transform bg-white hover:rounded-md hover:bg-primary hover:text-white hover:shadow hover:-translate-y-10 text-primary"
-                    initial={{ opacity: 0, y: getInitialY() }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5, delay: 1.5 }}
-                    key={`cta-${currentSlide}`}
-                  >
-                    <span className="my-auto"> {item.cta}</span>
-                    <FaAngleRight className='my-auto' />
-                  </motion.a>
-                </div>
-              </div>
-            </motion.div>
-          ))}
-        </Slider>
-      </div>
-    </section>
-  );
+	return (
+		<section className="text-white ">
+			<div className="pt-0 mx-auto text-center">
+				<Slider {...settings}>
+					{carouselItems.map((item, index) => (
+						<motion.div
+							key={index}
+							className="relative 2xl:h-[700px] h-[500px]  items-start grid place-content-end pb-10 overflow-hidden"
+							initial={{ scale: 0.9 }}
+							animate={{ scale: 1 }}
+							transition={{ duration: 0.5 }}
+						>
+							<Image
+								src={item.background.src}
+								alt={item.title}
+								layout="fill"
+								objectFit="cover"
+								className="absolute inset-0 z-[-1]"
+							/>
+							<div className="absolute inset-0 bg-black opacity-50 z-[-1]" />
+							<div className="relative z-10 my-auto text-start">
+								<div className="px-6 containers">
+									<motion.h1
+										className="mb-4 text-3xl font-bold md:text-5xl"
+										initial={{ opacity: 0, y: getInitialY() }}
+										animate={{ opacity: 1, y: 0, scale: [ 1, 1.2, 1 ] }}
+										transition={{ duration: 0.5, delay: 0.5, type: 'spring', stiffness: 100 }}
+										key={`title-${currentSlide}`}
+									>
+										{item.title}
+									</motion.h1>
+									<motion.p
+										className="mb-4 text-lg md:text-xl"
+										initial={{ opacity: 0, y: getInitialY() }}
+										animate={{ opacity: 1, y: 0 }}
+										transition={{ duration: 0.5, delay: 1 }}
+										key={`description-${currentSlide}`}
+									>
+										{item.description}
+									</motion.p>
+									<motion.a
+										href={item.link}
+										className="flex gap-1 mb-4 font-bold tracking-wider text-md hover:underline"
+										// className="px-6 py-3 text-lg font-bold transition-all transform bg-white hover:rounded-md hover:bg-primary hover:text-white hover:shadow hover:-translate-y-10 text-primary"
+										initial={{ opacity: 0, y: getInitialY() }}
+										animate={{ opacity: 1, y: 0 }}
+										transition={{ duration: 0.5, delay: 1.5 }}
+										key={`cta-${currentSlide}`}
+									>
+										<span className="my-auto"> {item.cta}</span>
+										<FaAngleRight className="my-auto" />
+									</motion.a>
+								</div>
+							</div>
+						</motion.div>
+					))}
+				</Slider>
+			</div>
+		</section>
+	);
 };
 
 export default Hero;

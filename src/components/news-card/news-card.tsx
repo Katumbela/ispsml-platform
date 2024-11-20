921fcad02e854d21bfecc965497ff8accc33887e
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-20 23:03:22 +0100

    fix: src/components/news-card/news-card.tsx. Modificação de código em src/components/news-card/news-card.tsx; Alteração de texto em src/components/news-card/news-card.tsx; Mudança de texto em um elemento HTML em src/components/news-card/news-card.tsx; Alteração de conteúdo em array ou objeto em src/components/news-card/news-card.tsx

---
+++ src/components/news-card/news-card.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import React from 'react';
+import { AbreviateString } from '../../utils/abreviate-utils';
+
+interface NewsCardProps {
+	title: string;
+	shortDescription: string;
+	postDate: string;
+	poster: string;
+	link: string;
+	image: string;
+}
+
+const NewsCard: React.FC<NewsCardProps> = ({ title, shortDescription, postDate, link, image }) => {
+	return (
+		<div className="w-[15rem] relative h-[21rem] overflow-hidden border shadow-lg">
+			<div className="relative w-full overflow-hidden h-44 imagem">
+				<Image
+					src={image}
+					objectFit="cover"
+					layout="fill"
+					className="transition-all hover:scale-105"
+					alt={title}
+				/>
+			</div>
+			<div className="p-2">
+				<h2 className="font-bold text-md">{AbreviateString.abbreviate(title, 40)}</h2>
+
+				<p className="mt-1 text-xs text-gray-400">{AbreviateString.abbreviate(shortDescription, 70)}</p>
+				<div className="absolute flex justify-between right-2 left-2 bottom-3">
+					<a
+						href={link}
+						className="px-3 py-1 mt-auto text-xs font-bold uppercase transition-all border hover:underline hover:bg-primary/10 text-primary"
+					>
+						Leia mais
+					</a>
+					<span className="text-xs">{postDate}</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default NewsCard;

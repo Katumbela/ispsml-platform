1af29b87681be4128ba7687e472630db5ff6b1ea
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-20 23:23:21 +0100

    fix: src/app/ispml-det/page.tsx. Modificação de código em src/app/ispml-det/page.tsx; Criação de uma constante em src/app/ispml-det/page.tsx; Alteração de conteúdo em array ou objeto em src/app/ispml-det/page.tsx

--- src/app/ispml-det/page.tsx
+++ src/app/ispml-det/page.tsx
@@ -1,18 +1,19 @@
 import Footer from '@/components/Footer';
-import Navbar from '../../components/Navbar'; 
+// import Navbar from '../../components/Navbar'; 
+import Navbar from '@/components/Navbar';
 import QuickLinks from '@/components/QuickLinks';
 import { PrepaSection } from './components/prepa-section';
 import { EngCoursesSection } from './components/eng-courses-section';
-import GlobalHero from '@/components/global-hero/global-hero';
-import { images } from '@/assets';
+// import GlobalHero from '@/components/global-hero/global-hero';
+// import { images } from '@/assets';
 
 const ISPSMLDet = () => {
   return (
     <div>
       <Navbar /> 
-      <GlobalHero  className='h-[400px] 2xl:h-[50px]' bottomBG='dark' bgImage={images.backgrounds.bg_night_1.src} title="   Departamento de Engenharia de Tecnologias" />
+      {/* <GlobalHero  className='h-[400px] 2xl:h-[50px]' bottomBG='dark' bgImage={images.backgrounds.bg_night_1.src} title="   Departamento de Engenharia de Tecnologias" /> */}
       
-      <main className="containers my-8">
+      <main className="my-8 containers">
         <p className="mt-4">
           O Departamento de Engenharia e Tecnologia da nossa instituição é dedicado a avançar as fronteiras do conhecimento e da inovação. Nossos programas são projetados para equipar os alunos com as habilidades e a expertise necessárias para se destacar em um cenário tecnológico em rápida evolução. Oferecemos uma variedade de cursos e oportunidades de pesquisa que cobrem várias áreas, como ciência da computação, engenharia elétrica, engenharia mecânica e mais. Nossos membros do corpo docente são especialistas renomados em suas áreas, comprometidos em fornecer educação de alta qualidade e promover um ambiente de aprendizado colaborativo. Junte-se a nós para fazer parte de uma comunidade que está moldando o futuro da engenharia e da tecnologia.
         </p>

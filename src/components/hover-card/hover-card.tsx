b4099055980c823e7731e6d02f34ccd4f683ac7e
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-20 23:03:21 +0100

    fix: src/components/hover-card/hover-card.tsx. Modificação de código em src/components/hover-card/hover-card.tsx; Alteração de texto em src/components/hover-card/hover-card.tsx; Mudança de texto em um elemento HTML em src/components/hover-card/hover-card.tsx; Adição de um novo estilo inline em src/components/hover-card/hover-card.tsx; Alteração de conteúdo em array ou objeto em src/components/hover-card/hover-card.tsx

--- src/components/hover-card/hover-card.tsx
+++ src/components/hover-card/hover-card.tsx
@@ -1,44 +1,47 @@
 import { motion } from 'framer-motion';
 
 interface HoverCardProps {
-  bgColor?: string;
-  bgImage?: string;
-  hoverBgColor: string;
-  title: string;
-  children: React.ReactNode;
-  className?: string;
+	bgColor?: string;
+	bgImage?: string;
+	hoverBgColor: string;
+	title: string;
+	children: React.ReactNode;
+	className?: string;
 }
 
 const HoverCard: React.FC<HoverCardProps> = ({ bgColor, bgImage, className, hoverBgColor, title, children }) => {
-  return (
-    <motion.div
-      className={`relative w-full grid h-64 ${bgColor} ${className} overflow-hidden cursor-pointer`}
-      initial={{ opacity: 1 }}
-      style={bgImage ? { backgroundImage: `url(${bgImage})`, backgroundSize: "cover", backgroundPosition: "center center"} : undefined}
-      whileHover={{ opacity: 1 }}
-      transition={{ duration: 0.3 }}
-    >
-      {/* Gradiente de baixo para cima cobrindo apenas 50% */}
-      <div className="absolute inset-x-0 bottom-0 flex items-end justify-start py-4 mt-auto h-1/2 bg-gradient-to-t from-black">
-       <div className="containers">
-       <h2 className="text-xl font-bold text-white">{title}</h2>
-       </div>
-      </div>
+	return (
+		<motion.div
+			className={`relative w-full grid h-64 ${bgColor} ${className} overflow-hidden cursor-pointer`}
+			initial={{ opacity: 1 }}
+			style={
+				bgImage ? (
+					{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', backgroundPosition: 'center center' }
+				) : (
+					undefined
+				)
+			}
+			whileHover={{ opacity: 1 }}
+			transition={{ duration: 0.3 }}
+		>
+			{/* Gradiente de baixo para cima cobrindo apenas 50% */}
+			<div className="absolute inset-x-0 bottom-0 flex items-end justify-start py-4 mt-auto h-1/2 bg-gradient-to-t from-black">
+				<div className="container px-5">
+					<h2 className="text-xl font-bold text-white">{title}</h2>
+				</div>
+			</div>
 
-      {/* Hover Content */}
-      <motion.div
-        className={`absolute inset-0 flex items-center justify-center ${hoverBgColor} transition duration-300 ease-in-out opacity-0 hover:opacity-100`}
-      >
-     <div className="containers">
-        {children}
-     </div>
-      </motion.div>
-    </motion.div>
-  );
+			{/* Hover Content */}
+			<motion.div
+				className={`absolute inset-0 flex items-center justify-center ${hoverBgColor} transition duration-300 ease-in-out opacity-0 hover:opacity-100`}
+			>
+				<div className="containers">{children}</div>
+			</motion.div>
+		</motion.div>
+	);
 };
 
 export default HoverCard;
-
 
 // <HoverCard
 //   bgColor="bg-blue-500"

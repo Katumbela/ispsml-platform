5866cb4a1c86fa9cd3ff6a521a6d1d1c95573fc7
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-20 23:03:17 +0100

    fix: src/components/Footer.tsx. Criação da função Footer em src/components/Footer.tsx; Criação de um botão em src/components/Footer.tsx; Modificação de código em src/components/Footer.tsx; Alteração de texto em src/components/Footer.tsx; Alteração em componente React em src/components/Footer.tsx; Mudança de texto em um elemento HTML em src/components/Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,66 +1,109 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-
-import { FaFacebook, FaInstagram, FaLinkedin, FaTiktok, FaXTwitter, FaYoutube } from "react-icons/fa6";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTiktok, FaXTwitter, FaYoutube } from 'react-icons/fa6';
+import { useTranslation } from 'react-i18next';
 
 export default function Footer() {
-  return (
-    <footer className="pt-20 text-white bg-[#212121]">
-      <div className="containers grid grid-cols-1 gap-8 mx-auto md:grid-cols-4">
-        <div>
-          <h3 className="mb-4 text-xl font-bold">Contato</h3>
-          <p>+244 123 456 789</p>
-          <p>+244 987 654 321</p>
-          <p>info@universidadeangolana.ao</p>
-          <p>São Martinho de Lima</p>
-          <p>Rua Exemplo, 123</p>
-          <p>Luanda, Angola</p>
-          <br />
-          <div className="flex gap-3">
-            <FaFacebook className="my-auto" />
-            <FaLinkedin className="my-auto" />
-            <FaXTwitter className="my-auto" />
-            <FaTiktok className="my-auto" />
-            <FaInstagram className="my-auto" />
-            <FaYoutube className="my-auto" />
-          </div>
-        </div>
-        <div>
-          <h3 className="mb-4 text-xl font-bold">Links Úteis</h3>
-          <ul>
-            <li><a href="/my-portal" className="hover:underline">Meu Portal ISPML</a></li>
-            <li><a href="/moodle" className="hover:underline">Moodle LMS</a></li>
-            <li><a href="/vacancies" className="hover:underline">Vagas</a></li>
-            <li><a href="/student-webmail" className="hover:underline">Webmail do Estudante</a></li>
-            <li><a href="/intranet" className="hover:underline">Intranet ISPML</a></li>
-          </ul>
-        </div>
-        <div>
-          <h3 className="mb-4 text-xl font-bold">Pesquisa e Inovação</h3>
-          <ul>
-            <li><a href="/research-services" className="hover:underline">Serviços de Pesquisa</a></li>
-            <li><a href="/journals" className="hover:underline">Jornais ISPML</a></li>
-            <li><a href="/repository" className="hover:underline">Repositório ISPML</a></li>
-            <li><a href="/press" className="hover:underline">Imprensa ISPML</a></li>
-            <li><a href="/grants" className="hover:underline">Aplicação de Subsídios</a></li>
-            <li><a href="/calendar" className="hover:underline">Calendário ISPML</a></li>
-          </ul>
-        </div>
-        <div>
-          <h3 className="mb-4 text-xl font-bold">Calendário ISPML</h3>
+    const { t } = useTranslation();
 
-          <button className="w-full py-3 text-sm transition-all bg-primary hover:bg-primary-dark">
-            Ver Calendário
-          </button>
-        </div>
-      </div>
-      <div className="py-4 mt-20 text-start bg-primary">
-        <div className="containers text-sm">
-
-          <p>&copy; 2024 São Martinho de Lima. Todos os direitos reservados.</p>
-
-        </div>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="pt-20 text-white bg-[#212121]">
+            <div className="grid grid-cols-1 gap-8 mx-auto containers md:grid-cols-4">
+                <div>
+                    <h3 className="mb-4 text-xl font-bold">{t('footer.contact')}</h3>
+                    <p>{t('footer.phone1')}</p>
+                    <p>{t('footer.phone2')}</p>
+                    <p>{t('footer.email')}</p>
+                    <p>{t('footer.addressLine1')}</p>
+                    <p>{t('footer.addressLine2')}</p>
+                    <p>{t('footer.city')}</p>
+                    <br />
+                    <div className="flex gap-3">
+                        <FaFacebook className="my-auto" />
+                        <FaLinkedin className="my-auto" />
+                        <FaXTwitter className="my-auto" />
+                        <FaTiktok className="my-auto" />
+                        <FaInstagram className="my-auto" />
+                        <FaYoutube className="my-auto" />
+                    </div>
+                </div>
+                <div>
+                    <h3 className="mb-4 text-xl font-bold">{t('footer.usefulLinks')}</h3>
+                    <ul>
+                        <li>
+                            <a href="/my-portal" className="hover:underline">
+                                {t('navbar.studyAtISPSML')}
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/moodle" className="hover:underline">
+                                {t('navbar.academicServices')}
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/vacancies" className="hover:underline">
+                                Vagas
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/student-webmail" className="hover:underline">
+                                Webmail do Estudante
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/intranet" className="hover:underline">
+                                Intranet ISPSML
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3 className="mb-4 text-xl font-bold">{t('footer.researchInnovation')}</h3>
+                    <ul>
+                        <li>
+                            <a href="/research-services" className="hover:underline">
+                                {t('navbar.research.policy')}
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/journals" className="hover:underline">
+                                Jornais ISPSML
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/repository" className="hover:underline">
+                                Repositório ISPSML
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/press" className="hover:underline">
+                                Imprensa ISPSML
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/grants" className="hover:underline">
+                                Aplicação de Subsídios
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/calendar" className="hover:underline">
+                                Calendário ISPSML
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3 className="mb-4 text-xl font-bold">{t('footer.calendar')}</h3>
+                    <button className="w-full py-3 text-sm transition-all bg-primary hover:bg-primary-dark">
+                        {t('footer.viewCalendar')}
+                    </button>
+                </div>
+            </div>
+            <div className="py-4 mt-20 text-start bg-primary">
+                <div className="text-sm containers">
+                    <p>{t('footer.allRightsReserved')}</p>
+                </div>
+            </div>
+        </footer>
+    );
 }

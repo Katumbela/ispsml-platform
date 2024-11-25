81d3d6975609d2fb0ee35c0ccbab28770b38c4ba
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-25 17:00:08 +0100

    fix: src/app/news/[slug]/page.tsx. Criação da função ViewNewsPage em src/app/news/[slug]/page.tsx; Modificação de código em src/app/news/[slug]/page.tsx; Alteração em componente React em src/app/news/[slug]/page.tsx; Alteração de conteúdo em array ou objeto em src/app/news/[slug]/page.tsx

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -91,7 +91,7 @@ export default function ViewNewsPage() {
                 <br />
 					<div className="grid grid-cols-4 gap-4 2xl:grid-cols-6">
 						{newsData
-							.slice(0, 4)
+							.slice(0, 6)
 							.map((news, index) => (
 								<NewsCard
 									key={index}

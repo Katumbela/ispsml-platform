241c2ba07d4e063e5e78c595e29228ff818b5a13
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-25 17:16:15 +0100

    fix: src/app/course/components/short_description_course.tsx. Criação da função ShortDescCourse em src/app/course/components/short_description_course.tsx; Modificação de código em src/app/course/components/short_description_course.tsx; Alteração de texto em src/app/course/components/short_description_course.tsx; Alteração em componente React em src/app/course/components/short_description_course.tsx; Mudança de texto em um elemento HTML em src/app/course/components/short_description_course.tsx

--- src/app/course/components/short_description_course.tsx
+++ src/app/course/components/short_description_course.tsx
@@ -9,7 +9,7 @@ export function ShortDescCourse({ course }: ShortDescCourseProps) {
     <div className="containers mt-[10vh]">
       <div className="flex">
         <div className="w-full">
-          <h1 className='text-2xl font-bold'>{course.course}</h1>
+          <h1 className='text-2xl font-semibold'>{course.course}</h1>
           <p className='text-slate-500'>{course.short_detail}</p>
           <p className='text-slate-500'>Duração: <b>{course.duration} anos</b></p>
           <p className='text-slate-500'>Turnos:

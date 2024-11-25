bbf5989c4a83270227f29a75779a3a62651110b0
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-25 17:16:31 +0100

    fix: src/components/Card.tsx. Modificação de código em src/components/Card.tsx; Alteração de texto em src/components/Card.tsx; Mudança de texto em um elemento HTML em src/components/Card.tsx; Alteração de conteúdo em array ou objeto em src/components/Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,7 +11,7 @@ const Card: React.FC<CardProps> = ({ image, title, description }) => {
     <div className="bg-white shadow-md rounded-lg overflow-hidden">
       <Image src={image} width={100} height={100} alt={title} className="w-full h-48 object-cover" />
       <div className="p-4">
-        <h3 className="text-xl font-bold mb-2">{title}</h3>
+        <h3 className="text-xl font-semibold mb-2">{title}</h3>
         <p>{description}</p>
       </div>
     </div>

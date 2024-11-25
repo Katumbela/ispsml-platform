6fc0ac403683c26b3f14a18edd86d72d1ed7a8b4
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-25 15:36:06 +0100

    fix: src/components/news-card/news-card.tsx. Modificação de código em src/components/news-card/news-card.tsx; Alteração de texto em src/components/news-card/news-card.tsx; Mudança de texto em um elemento HTML em src/components/news-card/news-card.tsx; Alteração de conteúdo em array ou objeto em src/components/news-card/news-card.tsx

--- src/components/news-card/news-card.tsx
+++ src/components/news-card/news-card.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 import { AbreviateString } from '../../utils/abreviate-utils';
+import { routes } from '@/infra/routes.vars'; 
 
 interface NewsCardProps {
 	title: string;
@@ -10,32 +11,37 @@ interface NewsCardProps {
 	poster: string;
 	link: string;
 	image: string;
+	slug: string;
 }
 
-const NewsCard: React.FC<NewsCardProps> = ({ title, longDescription, postDate, link, image }) => {
+const NewsCard: React.FC<NewsCardProps> = ({ slug, title, longDescription, postDate, image }) => {
 	return (
-		<div className="w-[14.5rem] relative h-[24rem] overflow-hidden border shadow-lg">
-			<div className="relative w-full overflow-hidden h-44 imagem">
-				<Image
-					src={image}
-					objectFit="cover"
-					layout="fill"
-					className="transition-all hover:scale-105"
-					alt={title}
-				/>
-			</div>
-			<div className="p-2">
-				<h2 className="font-bold text-md">{AbreviateString.abbreviate(title, 50)}</h2>
+		<div className="px-1 2xl:px-2">
+			<div className="w-full relative h-[24rem] 2xl:h-[45vh] overflow-hidden border shadow-lg">
+				<div className="relative w-full overflow-hidden h-44 2xl:h-[25vh] imagem">
+					<Image
+						src={image}
+						objectFit="cover"
+						layout="fill"
+						className="transition-all hover:scale-105"
+						alt={title}
+					/>
+				</div>
+				<div className="p-2">
+					<h2 className="font-bold text-md 2xl:text-lg">{AbreviateString.abbreviate(title, 50)}</h2>
 
-				<p className="mt-1 text-xs text-gray-400">{AbreviateString.abbreviate(longDescription, 120)}</p>
-				<div className="absolute flex justify-between right-2 left-2 bottom-3">
-					<a
-						href={link}
-						className="px-3 py-1 mt-auto text-xs font-bold uppercase transition-all border hover:underline hover:bg-primary/10 text-primary"
-					>
-						Leia mais
-					</a>
-					<span className="text-xs">{postDate}</span>
+					<p className="mt-1 text-xs text-gray-400 2xl:text-md">
+						{AbreviateString.abbreviate(longDescription, 120)}
+					</p>
+					<div className="absolute flex justify-between right-2 left-2 bottom-3">
+						<a
+							href={routes.VIEW_NEWS_ROUTE + '/' + slug}
+							className="px-3 py-1 mt-auto text-xs font-bold uppercase transition-all border hover:underline hover:bg-primary/10 text-primary"
+						>
+							Leia mais
+						</a>
+						<span className="text-xs">{postDate}</span>
+					</div>
 				</div>
 			</div>
 		</div>

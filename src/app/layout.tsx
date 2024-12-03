6b22b3410894c2de31047873998c9660323ca47f
Joao Afonso Katombela <ja3328173@gmail.com>
2024-12-03 13:09:17 +0100

    fix: src/app/layout.tsx. Criação da função RootLayout em src/app/layout.tsx; Modificação de código em src/app/layout.tsx; Alteração de conteúdo em array ou objeto em src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,9 @@ export default function RootLayout({
 
   return (
     <html lang={lang}>
+      <head>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=contacts" />
+      </head>
       <body
         // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

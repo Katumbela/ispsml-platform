e235daedfe87789881d06ecfa00f75af0cce89d4
Joao Afonso Katombela <ja3328173@gmail.com>
2024-12-03 15:26:34 +0100

    feat: src/infra/data/admission-datas.tsx. Criação de novo arquivo

---
+++ src/infra/data/admission-datas.tsx
@@ -0,0 +1,58 @@
+import { CardData } from '@/app/admissions-and-scholarships/components/admissions-card';
+import { FaWallet, FaWpforms } from 'react-icons/fa6';
+
+export const cardData: CardData[] = [
+	{
+		iconType: FaWpforms,
+		title: 'Processo de Inscrição'
+	},
+	{
+		iconType: FaWpforms,
+		title: 'Saiba sobre o Prepa'
+	},
+	{
+		iconType: FaWpforms,
+		title: 'Oferta Educacional'
+	},
+	{
+		iconType: FaWpforms,
+		title: 'Diferencial'
+	},
+	{
+		iconType: FaWpforms,
+		title: 'Contacte nos'
+	},
+	
+];
+
+
+export const scholarShipsData: CardData[] = [
+	{
+		iconType: FaWallet,
+		title: 'Bolsa de SOcioeconomia'
+	},
+	{
+		iconType: FaWallet,
+		title: 'Bolsa CTEM'
+	},
+	{
+		iconType: FaWallet,
+		title: 'Bolsa de Estudos Academico'
+	},
+	{
+		iconType: FaWallet,
+		title: 'Bolsa CTEM'
+	},
+	// {
+	// 	iconType: FaWallet,
+	// 	title: 'Bolsa CTEM'
+	// },
+	// {
+	// 	iconType: FaWallet,
+	// 	title: 'Bolsa de Estudos Academico'
+	// },
+	// {
+	// 	iconType: FaWallet,
+	// 	title: 'Bolsa CTEM'
+	// }
+];

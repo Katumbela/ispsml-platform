067896c4c1f5b6fd937c0f52f12f341a26f93a9a
Joao Afonso Katombela <ja3328173@gmail.com>
2024-12-03 12:53:01 +0100

    fix: src/app/admissions-and-scholarships/page.tsx. Criação da função OrganicUnitPage em src/app/admissions-and-scholarships/page.tsx; Criação de um botão em src/app/admissions-and-scholarships/page.tsx; Modificação de código em src/app/admissions-and-scholarships/page.tsx; Alteração de texto em src/app/admissions-and-scholarships/page.tsx; Criação de uma constante em src/app/admissions-and-scholarships/page.tsx; Alteração em componente React em src/app/admissions-and-scholarships/page.tsx; Mudança de texto em um elemento HTML em src/app/admissions-and-scholarships/page.tsx; Alteração de conteúdo em array ou objeto em src/app/admissions-and-scholarships/page.tsx

--- src/app/admissions-and-scholarships/page.tsx
+++ src/app/admissions-and-scholarships/page.tsx
@@ -6,6 +6,11 @@ import Footer from '@/components/Footer';
 // import { coursesData } from '@/infra/data/courses-data';
 import GlobalHero from '@/components/global-hero/global-hero';
 import { images } from '@/assets';
+import { motion } from 'framer-motion';
+import { FaAngleRight } from 'react-icons/fa6';
+import Image from 'next/image';
+// import Head from 'next/head';
+// import { env } from '@/infra/env';
 
 // const departments = Object.entries(coursesData).map(([key, value]) => ({
 //   link: `${routes.ORGANIC_UNIT_ROUTE}/${key}`,
@@ -15,12 +20,51 @@ import { images } from '@/assets';
 
 export default function OrganicUnitPage() {
   return (
-    <div>
+    <>
+    <head>
+			<title>Inscrições e Bolsas de Estudos | Instituto Superior Politécnico São Martinho de Lima </title>
+		</head>
       <Navbar />
-      <GlobalHero bgImage={images.backgrounds.bg_prepa_tec.src} titleClassName='text-6xl' title='Inscrições e Bolsas' className='h-[600px] 2xl:h-[700px] ' />
+      <GlobalHero bgImage={images.backgrounds.bg_prepa_tec.src} titleClassName='text-6xl' title='Inscrições e Bolsas' className='h-[500px] 2xl:h-[600px] ' />
       <br />
+      <div className="containers">
+        <p className="text-gray-500">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima sapiente facere impedit est voluptatum quidem quam iste similique eum repellendus, mollitia alias dolor reiciendis atque exercitationem ut deleniti velit quisquam?
+        </p>
+        </div>
+        <br />
+        <br />
+        <motion.div
+				initial={{ x: -1000, opacity: 0 }}
+				viewport={{ once: true }}
+				whileInView={{ x: 0, opacity: 1, transition: { duration: 0.3, delay: 0.1 } }}
+				className=" bg-primary-footer image-container"
+			>
+				<div className="flex gap-2 ">
+					<div className="w-1/2 ps-10 py-14 2xl:py-24">
+						<h2 className="font-bold text-white lg:mb-2 lg:text-3xl 2xl:mb-3 2xl:text-4xl">
+							 Relações Empresariais
+						</h2>
+						<p className="text-sm 2xl:text-[14px] text-white">At Tec we connect and collaborate with national and international organizations through valuable corporate relationships, through a bonding model that generates human, economic, social, and environmental value to contribute to a fairer, more prosperous, and more sustainable society with higher levels of wellness.</p>
+						<button className="flex gap-2 py-5 mt-6 font-semibold text-black uppercase transition-all bg-white border-2 border-white px-7 hover:bg-transparent hover:text-white ">
+							<span className="my-auto">Saber Mais</span>
+							<FaAngleRight className="my-auto" />
+						</button>
+					</div>
+					<div className="relative w-1/2 h-100 image-container">
+						<Image
+							objectFit="cover"
+							layout="fill"
+							alt=""
+							src={images.backgrounds.bg_books_pen.src}
+							className="image"
+						/>
+					</div>
+				</div>
+			</motion.div>
+
       
       <Footer />
-    </div>
+    </>
   );
 }

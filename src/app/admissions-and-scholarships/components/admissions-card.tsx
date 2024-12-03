ddb1cffc650e6d28f9a85fb497bda841e65d67ef
Joao Afonso Katombela <ja3328173@gmail.com>
2024-12-03 15:40:09 +0100

    fix: src/app/admissions-and-scholarships/components/admissions-card.tsx. Criação da função AdmissionsCard em src/app/admissions-and-scholarships/components/admissions-card.tsx; Modificação de código em src/app/admissions-and-scholarships/components/admissions-card.tsx; Alteração de texto em src/app/admissions-and-scholarships/components/admissions-card.tsx; Alteração em componente React em src/app/admissions-and-scholarships/components/admissions-card.tsx; Alteração de conteúdo em array ou objeto em src/app/admissions-and-scholarships/components/admissions-card.tsx

--- src/app/admissions-and-scholarships/components/admissions-card.tsx
+++ src/app/admissions-and-scholarships/components/admissions-card.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { IconType } from 'react-icons';
+import cn from "classnames"
 
 export interface CardData {
 	iconType: IconType;
 	title: string;
 	link?: string;
-	type?: 'admission' | 'undergraduate' | 'graduate';
 }
 
 export interface IACard {
 	card: CardData;
+	type?: 'admission' | 'undergraduate' | 'graduate';
 }
 
-export default function AdmissionsCard({ card }: IACard) {
+export default function AdmissionsCard({ card, type }: IACard) {
 	return (
-		<div className="grid items-center cursor-pointer p-5 w-[16rem] h-[17rem] bg-primary hover:bg-primary-light">
+		<div className={cn("grid items-center cursor-pointer p-5 w-[16rem] h-[17rem] ", {"bg-primary hover:bg-primary-light": !type, "bg-primary-footer hover:bg-primary-dark": type == "undergraduate"  })}>
 			<div className="text-center">
 				<div className="text-4xl text-white">
 					{React.createElement(card.iconType, { className: 'text-7xl font-medium mx-auto' })}

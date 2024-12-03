091b0767706adc2184c3f750c41673551d42aa35
Joao Afonso Katombela <ja3328173@gmail.com>
2024-12-03 17:46:46 +0100

    fix: src/app/contact/page.tsx. Modificação de código em src/app/contact/page.tsx; Criação de uma constante em src/app/contact/page.tsx; Alteração de conteúdo em array ou objeto em src/app/contact/page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,27 +1,27 @@
-"use client"
+'use client';
 
 import GlobalHero from '@/components/global-hero/global-hero';
-import Navbar from '../../components/Navbar';
 import { images } from '@/assets';
 import ContactComponent from './components/contact-component';
-import Footer from '@/components/Footer';
 
 const Contact = () => {
-  return (
-    <div>
-      <Navbar />
-      <GlobalHero title='Como podemos ajudar ?' subtitle='Contante o ISPSML' className='h-[500px] 2xl:h-[650px]' bottomBG='dark' bgImage={images.backgrounds.bg_support.src}/>
-      <main className="containers">
-      
-      <ContactComponent />
+	return (
+		<div>
+			<GlobalHero
+				title="Como podemos ajudar ?"
+				subtitle="Contante o ISPSML"
+				className="h-[500px] 2xl:h-[650px]"
+				bottomBG="dark"
+				bgImage={images.backgrounds.bg_support.src}
+			/>
+			<main className="containers">
+				<ContactComponent />
 
-<br />
-<br />
-      
-      </main>
-<Footer />
-    </div>
-  );
+				<br />
+				<br />
+			</main>
+		</div>
+	);
 };
 
 export default Contact;

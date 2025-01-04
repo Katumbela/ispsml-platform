c96c948642a82ac9251d9727d46ee43bb64550d1
Joao Afonso Katombela <ja3328173@gmail.com>
2025-01-04 13:35:27 +0100

    fix: src/components/cards-science-secton/cards-science-section.tsx. Criação da função ScienceStudyCard em src/components/cards-science-secton/cards-science-section.tsx; Criação de um botão em src/components/cards-science-secton/cards-science-section.tsx; Modificação de código em src/components/cards-science-secton/cards-science-section.tsx; Alteração de texto em src/components/cards-science-secton/cards-science-section.tsx; Alteração em componente React em src/components/cards-science-secton/cards-science-section.tsx; Mudança de texto em um elemento HTML em src/components/cards-science-secton/cards-science-section.tsx; Alteração de conteúdo em array ou objeto em src/components/cards-science-secton/cards-science-section.tsx

--- src/components/cards-science-secton/cards-science-section.tsx
+++ src/components/cards-science-secton/cards-science-section.tsx
@@ -1,10 +1,15 @@
 import { images } from '@/assets';
-import { HoverCard } from '../hover-card/hover-card';
+// import { HoverCard } from '../hover-card/hover-card';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import { routes } from '@/infra/routes.vars';
+import ButtonBlueComponent from '../button-blue/button-blue-component';
+
 
 export function ScienceStudyCard() {
 	return (
-		<section className="pt-[6vh] pb-1 ">
-		
+		<section className="mt-[2rem] containers pb-1 ">
+
 			{/* <div className="flex my-1 containers bg-primary-footer">
 				<div className="">
 					<div className="">
@@ -23,8 +28,56 @@ export function ScienceStudyCard() {
 				</div>
 			</div> */}
 
+			<motion.div
+				initial={{ x: -1000, opacity: 0 }}
+				viewport={{ once: false }}
+				whileInView={{ x: 0, opacity: 1, transition: { duration: 0.3, delay: 0.1 } }}
+				className="flex flex-row-reverse bg-white">
+				<div className="relative w-full py-10 bg-primary">
+					<Image objectPosition='top' src={images.bgsSchool.bg_school_27.src} objectFit='cover' layout='fill' alt="" />
+				</div>
+				<div className="grid items-center w-full py-10 bg-white 2xl:py-20 ">
+					<div className="my-auto ">
+						<h2 className='title'>Laboratórios de Informática</h2>
+						<p className="mt-4 text-gray-600">
+							Conheça a nova abordagem de pesquisa no Tecnológico de Monterrey. Descubra os três
+							institutos e os diferentes Core Labs que mudarão a forma de realizar pesquisa no México.
+						</p>
+						<ButtonBlueComponent
+							onclick={() => window.location.href = routes.SOCIAL_REPONSABILITY_ROUTE}
+							text="Saber mais" />
+						{/* <button className="flex gap-2 py-5 mt-10 text-white uppercase transition-all bg-black border-2 border-black px-7 hover:bg-white hover:text-black ">
+							<span className="my-auto">Saber Mais</span>
+							<FaAngleRight className="my-auto" />
+						</button> */}
+					</div>
+				</div>
+			</motion.div>
+
+			<motion.div
+				initial={{ x: 1000, opacity: 0 }}
+				viewport={{ once: false }}
+				whileInView={{ x: 0, opacity: 1, transition: { duration: 0.3, delay: 0.1 } }}
+				className="flex flex-row bg-white">
+				<div className="relative w-full py-10 bg-primary">
+					<Image objectPosition='top' src={images.bgsSchool.bg_school_16.src} objectFit='cover' layout='fill' alt="" />
+				</div>
+				<div className="grid items-center w-full py-10 bg-white 2xl:py-20 ">
+					<div className="my-auto containers ">
+						<h2 className='title'>Biblioteca Moderna</h2>
+						<p className="mt-4 text-gray-600">
+							Conheça a nova abordagem de pesquisa no Tecnológico de Monterrey. Descubra os três
+							institutos e os diferentes Core Labs que mudarão a forma de realizar pesquisa no México.
+						</p>
+						<ButtonBlueComponent
+							onclick={() => window.location.href = routes.SOCIAL_REPONSABILITY_ROUTE}
+							text="Saber mais" />
+					</div>
+				</div>
+			</motion.div>
+
 			<div className="flex gap-1">
-				<HoverCard
+				{/* <HoverCard
 					bgColor="bg-primary"
 					hoverBgColor="bg-primary"
 					title="Laboratório de Informática"
@@ -42,8 +95,8 @@ export function ScienceStudyCard() {
 							Saber mais
 						</a>
 					</div>
-				</HoverCard>
-				<HoverCard
+				</HoverCard> */}
+				{/* <HoverCard
 					bgImage={images.bgsSchool.bg_school_16.src}
 					className="h-[400px] 2xl:h-[600px]"
 					hoverBgColor="bg-red-500"
@@ -60,7 +113,7 @@ export function ScienceStudyCard() {
 							Saber mais
 						</a>
 					</div>
-				</HoverCard>
+				</HoverCard> */}
 			</div>
 		</section>
 	);

bb5c54e8e2cf3df9c846fdc85b06c2a8444ce260
Joao Afonso Katombela <ja3328173@gmail.com>
2025-01-04 13:35:17 +0100

    fix: src/app/organic-unit/[department]/[course_slug]/page.tsx. Criação da função CourseDetailsPage em src/app/organic-unit/[department]/[course_slug]/page.tsx; Criação de um botão em src/app/organic-unit/[department]/[course_slug]/page.tsx; Adição de log no console em src/app/organic-unit/[department]/[course_slug]/page.tsx; Modificação de código em src/app/organic-unit/[department]/[course_slug]/page.tsx; Alteração de texto em src/app/organic-unit/[department]/[course_slug]/page.tsx; Criação de uma constante em src/app/organic-unit/[department]/[course_slug]/page.tsx; Alteração em componente React em src/app/organic-unit/[department]/[course_slug]/page.tsx; Mudança de texto em um elemento HTML em src/app/organic-unit/[department]/[course_slug]/page.tsx; Alteração em funções de manipulação de eventos em src/app/organic-unit/[department]/[course_slug]/page.tsx; Alteração de conteúdo em array ou objeto em src/app/organic-unit/[department]/[course_slug]/page.tsx

--- src/app/organic-unit/[department]/[course_slug]/page.tsx
+++ src/app/organic-unit/[department]/[course_slug]/page.tsx
@@ -10,11 +10,12 @@ import { ShortDescCourse } from '../../components/short_description_course';
 import { FaAngleRight, FaDownload } from 'react-icons/fa6';
 // import QuickLinks from '@/components/QuickLinks';
 import { routes } from '@/infra/routes.vars';
+import { AbreviateString } from '@/utils';
 // import { RolesData } from '@/infra/data/roles-data';
 
 export default function CourseDetailsPage() {
   const { department, course_slug } = useParams();
-	const departmentData = coursesData[department as string];
+  const departmentData = coursesData[department as string];
 
   const course = typeof department === 'string' ? coursesData[department]?.courses.find(c => c.slug === course_slug) : null;
   const [openYear, setOpenYear] = useState<number | null>(null);
@@ -27,18 +28,18 @@ export default function CourseDetailsPage() {
     setOpenYear(openYear === year ? null : year);
   };
 
-  
-// function getCourseTeachers(courseId: string) {
-//   const course = coursesData.find((course: any) => course.id === courseId)
-//   if (course) {
-//       return course.teachers.map((teacherId: any) => RolesData.find(role => role.id === teacherId))
-//   }
-//   return []
-// }
 
-// // Exemplo de uso
-// const courseTeachers = getCourseTeachers("curso-biologia")
-// console.log(courseTeachers)
+  // function getCourseTeachers(courseId: string) {
+  //   const course = coursesData.find((course: any) => course.id === courseId)
+  //   if (course) {
+  //       return course.teachers.map((teacherId: any) => RolesData.find(role => role.id === teacherId))
+  //   }
+  //   return []
+  // }
+
+  // // Exemplo de uso
+  // const courseTeachers = getCourseTeachers("curso-biologia")
+  // console.log(courseTeachers)
 
   return (
     <>
@@ -46,20 +47,30 @@ export default function CourseDetailsPage() {
         <title>{course.course} | ISPSML</title>
         <meta name="description" content={`Saiba mais sobre o curso de ${course.course} na ISPSML.`} />
         <meta name="keywords" content={`${course.course}, ISPSML, cursos`} />
-      </head> 
+      </head>
       <HeroCourseDetail departmentName={departmentData.name as string} department={department as string} course={course} bg_image={course.course_cover} title={course.course} />
       <ShortDescCourse course={course} />
       <br />
       <br />
       <br />
       <div className="containers">
-        <h2 className='mb-6 text-3xl font-semibold'>Benefícios do Programa</h2>
         <div className="grid grid-cols-2 gap-4">
-            {course.benefits?.map((benefit: string, index: number) => (
-            <div key={index} className="flex gap-2 benefit-item">
-             <div className="w-3 h-3 my-auto border-2 rounded-full border-primary/70"></div>  <p>{benefit}</p>
-            </div>
+          <div className="p">
+            <h2 className="title-course">Perfil de Entrada</h2>
+            {course.entryProfile?.slice(0, 3).map((item: string, index: number) => (
+              <div key={index} className="flex gap-2 benefit-item">
+                <div className="w-3 h-3 my-auto border-2 rounded-full border-primary/70"></div>  <p>{AbreviateString.abbreviate(item, 40)}</p>
+              </div>
             ))}
+          </div>
+          <div className="p">
+            <h2 className="title-course">Perfil de Saída</h2>
+            {course.outProfile?.map((item: string, index: number) => (
+              <div key={index} className="flex gap-2 benefit-item">
+                <div className="w-3 h-3 my-auto border-2 rounded-full border-primary/70"></div>  <p>{AbreviateString.abbreviate(item, 40)}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <br />
@@ -82,23 +93,23 @@ export default function CourseDetailsPage() {
       <br />
       <br />
 
-      <div  className="flex justify-between containers">
-      <div  className="flex justify-between w-full ">
-        <div className="w-full my-auto">
-          <h2 className="text-2xl font-semibold">
-            Baixar Plano Curricular de {course.course}
-          </h2>
+      <div className="flex justify-between containers">
+        <div className="flex justify-between w-full ">
+          <div className="w-full my-auto">
+            <h2 className="text-2xl font-semibold">
+              Baixar Plano Curricular de {course.course}
+            </h2>
+          </div>
+          <div className="w-full my-auto">
+            <FaDownload className='my-auto text-xl' />
+          </div>
         </div>
-        <div className="w-full my-auto">
-          <FaDownload className='my-auto text-xl' />
+        <div className="flex justify-end w-full text-end ">
+          <button onClick={() => window.location.href = routes.APPLY_ROUTE + "?course=" + course.course} className="flex gap-2 py-5 mt-10 text-white uppercase transition-all bg-black border-2 border-black px-7 hover:bg-white hover:text-black ">
+            <span className="my-auto">Inscrever-se</span>
+            <FaAngleRight className="my-auto" />
+          </button>
         </div>
-      </div>
-      <div  className="flex justify-end w-full text-end ">
-      <button  onClick={()=> window.location.href = routes.APPLY_ROUTE+"?course="+course.course}  className="flex gap-2 py-5 mt-10 text-white uppercase transition-all bg-black border-2 border-black px-7 hover:bg-white hover:text-black ">
-							<span className="my-auto">Inscrição</span>
-							<FaAngleRight className="my-auto" />
-						</button>
-      </div>
       </div>
       <br />
       <br />

10a01b82d4ef7decce0e39bde232844cc1fd6338
Joao Afonso Katombela <ja3328173@gmail.com>
2025-02-18 12:40:49 +0100

    fix: change postgres by mysql

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     // output: "export",
     env: {
-        DATABASE_URL: "postgresql://ispsmlao_katumbela:Katumbela2025@65.108.218.149:5432/ispsmlao_db",
+        DATABASE_URL: "mysql://ispsmlao_katumbela:Joao2025@65.108.218.149:3306/ispsmlao_dev",
         SHADOW_DATABASE_URL: "mysql://ispsmlao_m2j_technnology:ispsml2025@65.108.218.149:3306/ispsmlao_ispsml_db"
     },
     images: {
